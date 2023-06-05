ddb8944c2a31f96287b55611e09e51bf08349e34
hunter <123750621+jack20232007@users.noreply.github.com>
2023-06-06 00:04:20 +0530

    done

---
+++ scri.js
@@ -0,0 +1,16 @@
+function yes() {
+    window.location.href="next.html"
+    
+}
+function insta(){
+    window.location.href="https://www.instagram.com/jumli_d/"
+
+}
+function android() {
+    window.location.href="camra.html"
+    
+}
+function last() {
+    window.location.href="last.html"
+    
+}
